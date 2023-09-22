9ec3a58bfa1bc00575dcd24e83f3626a46c99bfa
sonamc123 <144881363+sonamc123@users.noreply.github.com>
2023-09-22 10:05:35 +0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
  public static void main(String[]args){
-  System.out.println("------*****Hello World & welcome to the jenkins training*****------");
+  System.out.println("------*****Made some changes*****------");
  }
 }
